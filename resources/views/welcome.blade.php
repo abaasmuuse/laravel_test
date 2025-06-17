a89d8a9363a15ff1412d3783df27d2a640358195
abaas <abasmusehashi@gma>
2025-06-17 16:12:21 +0300

    canab

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,7 +89,7 @@
                                 </span>
                             </span>
                             <span>
-                               abaas muuse haashi
+                               canab abdulkadri 
                                 <a href="https://laracasts.com" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
                                     <span>Laracasts</span>
                                     <svg
